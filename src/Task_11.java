316586a641a00ede0b8133850a0c229807915fc5
asamokhina94 <cdfiY#16Z>
2020-04-22 12:15:46 +0300

    Initial commit

---
+++ src/Task_11.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Task_11 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String str = in.nextLine(); // запись введённой строки в строковую переменную
+        System.out.println("Вы ввели: " + str);
+    }
+}
+
